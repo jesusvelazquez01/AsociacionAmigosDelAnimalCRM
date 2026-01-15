25844800e5b6a75d530808a31686c195f6e69365
jesus <jv176118@gmail.com>
2026-01-15 09:49:28 -0300

    Cambios de frontend para dockerrizar en vps

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api',
+    withCredentials: true,  // Necesario para CORS con credentials
 
     headers: {
         'Content-Type': 'application/json',
